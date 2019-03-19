af011fdbc4aeff04284db949654ca2563efa4889
Mohamed Reda <36422290+Mohamedrredaa@users.noreply.github.com>
2019-03-19 23:07:39 +0200

    Delete LCDDD.h

--- LCD Driver/LCDDD.h
+++
@@ -1,9 +0,0 @@
-void delay(unsigned int x);
-void LCD_Init_Pins();
-void LCD_CMD(unsigned int cmd);
-void LCD_Init();
-void Display_Char(char letter,unsigned int position);
-void Display_str(char* str);
-
-
-
